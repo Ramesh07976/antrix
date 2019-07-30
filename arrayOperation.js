0e8ecb22a1ba87a4f87a87833a111253978f73a7
Ramesh07976 <53205965+Ramesh07976@users.noreply.github.com>
2019-07-30 10:08:35 +0530

    Extract elements from array

--- arrayOperation.js
+++ arrayOperation.js
@@ -2,8 +2,15 @@
 
 let myArr = [3, 5, 6, 7, 9];
 let another = myArr.pop();
-
 console.log(another);
+
+//Extract elements from array
+let myArr = [3, 5, 6, 7, 9]; //this is array
+console.log(myArr.pop()); // Extract 9
+console.log(myArr);       //Remaining element in array[3,5,6,7]
+console.log(myArr.pop());// Extract 7
+console.log(myArr);        //Remaining element in array[3,5,6]
+console.log(myArr.pop()); //// Extract 6
 
 // Extract first element from start of array
 

aa7ee6462e59801d67b61a8142542e103e902667
Ramesh07976 <53205965+Ramesh07976@users.noreply.github.com>
2019-07-30 09:45:20 +0530

    Update short.js

--- short.js
+++ short.js
@@ -4,6 +4,12 @@ let myArr = [34, 66, 63, 87, 10];
 let sortedArry = myArr.sort((a, b) => a > b ? 1 : -1);
 console.log(sortedArry);
 
+//Array sort 2nd Method
+let myarray =[78,90,57,68,90,878,575];
+console.log(myarray.sort((a,b)=>{
+    return a<b;
+}));
+
 
 // Array unique value find 
 
